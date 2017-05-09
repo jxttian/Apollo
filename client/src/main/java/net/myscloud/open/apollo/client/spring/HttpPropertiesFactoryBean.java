f724ed346a2c3f781c9a57843eddceaa7f81ac86
Genesis <jxttian@gmail.com>
2017-05-09 19:00:54 +0800

    windows 下 bug fix

--- client/src/main/java/net/myscloud/open/apollo/client/spring/HttpPropertiesFactoryBean.java
+++ client/src/main/java/net/myscloud/open/apollo/client/spring/HttpPropertiesFactoryBean.java
@@ -17,6 +17,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -56,12 +57,12 @@ public class HttpPropertiesFactoryBean extends PropertiesFactoryBean {
         OkHttpClient client = new OkHttpClient();
         List<Resource> resources = Lists.newArrayList();
         if (StringKits.isNotEmpty(name)) {
-            resources.add(loadRemoteProperties(client, name, props, apolloProp));
+            resources.add(loadRemoteProperties(client, name, apolloProp));
         }
         if (CollectionKits.isNotEmpty(names)) {
             names.stream()
                     .filter(StringKits::isNotEmpty)
-                    .forEach(item -> resources.add(loadRemoteProperties(client, item, props, apolloProp)));
+                    .forEach(item -> resources.add(loadRemoteProperties(client, item, apolloProp)));
         }
         Resource[] tmp = new Resource[resources.size()];
         resources.toArray(tmp);
@@ -69,7 +70,7 @@ public class HttpPropertiesFactoryBean extends PropertiesFactoryBean {
         super.loadProperties(props);
     }
 
-    private Resource loadRemoteProperties(OkHttpClient client, String name, Properties props, Properties apolloProp) {
+    private Resource loadRemoteProperties(OkHttpClient client, String name, Properties apolloProp) {
         String mode = apolloProp.getProperty("apollo.mode");
         if (StringKits.isEmpty(mode) || !"local".equals(mode)) {
             //非本地模式运行
@@ -82,23 +83,27 @@ public class HttpPropertiesFactoryBean extends PropertiesFactoryBean {
             Request request = new Request.Builder()
                     .header("certification", apolloProp.getProperty("apollo.certification", ""))
                     .url(url).build();
-            try {
-                try (Response response = client.newCall(request).execute()) {
-                    String content = response.body().string();
-                    JSONObject result = JSON.parseObject(content);
-                    if (result.getInteger("code") == net.myscloud.open.apollo.common.framework.Response.Status.SUCCESS.getCode()) {
-                        String configContent = result.getString("data");
-                        log.info("加载远程配置文件成功 Content:[{}]", configContent);
-                        URL pathUrl = Thread.currentThread().getContextClassLoader().getResource("/");
-                        if (pathUrl != null) {
-                            //将配置文件保存到ClassPath
-                            Files.write(Paths.get(pathUrl.getPath() + name), configContent.getBytes(Charset.defaultCharset()));
-                        } else {
-                            log.warn("获取ClassPath失败");
-                        }
-                    } else {
-                        log.warn("加载远程配置文件失败，Error:{}", result.getString("msg"));
-                    }
+            URL pathUrl = Thread.currentThread().getContextClassLoader().getResource("/");
+            if (pathUrl == null) {
+                log.warn("获取ClassPath失败");
+                return new ClassPathResource(name);
+            }
+            //Windows环境获取ClassPath后需特殊处理
+            String classPath = System.getProperty("os.name").toLowerCase().startsWith("win")
+                    ? pathUrl.getPath().substring(1) : pathUrl.getPath();
+            log.info("获取ClassPath[{}]成功", classPath);
+            try (Response response = client.newCall(request).execute()) {
+                String content = response.body().string();
+                JSONObject result = JSON.parseObject(content);
+                if (result.getInteger("code") == net.myscloud.open.apollo.common.framework.Response.Status.SUCCESS.getCode()) {
+                    String configContent = result.getString("data");
+                    log.info("加载远程配置文件成功 Content:[{}]", configContent);
+                    //将配置文件保存到ClassPath
+                    Files.write(Paths.get(classPath + name)
+                            , configContent.getBytes(Charset.defaultCharset()));
+
+                } else {
+                    log.warn("加载远程配置文件失败，Error:{}", result.getString("msg"));
                 }
             } catch (IOException e) {
                 log.warn(e.getMessage(), e);
